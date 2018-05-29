3237c09f214949b0203f5230a60bf6cb6b65bea9
Chary1729 <39719416+Chary1729@users.noreply.github.com>
2018-05-29 09:56:09 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,11 @@
+let name = "uday"
+
+let score =100
+console.log(score);
+let bo = 399
+let my= score+bo
+console.log(my);
+let firstName = "uday"
+let lastName = "kumarchary"
+console.log(firstName +lastName);
+tempInFahrenheit =100
